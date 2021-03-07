197db3f138ac5a5d54765c461e9b4bcee2ab8c61
Gourav8152-ai <goravsitbatch1@gmail.com>
2021-03-08 02:54:15 +0530

    Addimg Js

---
+++ js/life.js
@@ -0,0 +1,22 @@
+let a = 10;
+console.log(a);
+let b = [5,4,3,2,1];
+console.log(b);
+console.log(b.length);
+console.log('Hello World');
+console.log("Hello World");
+
+
+// Varaiable
+c = 5;   //Global Variable
+var d = 6;  //Function Variable
+let e = 7;   //BLock Variable
+
+
+//function
+function name(n) {
+    console.log("My name is "+ n);
+}
+let f = function() {
+    console.log("Your name is IDK!!")
+}
